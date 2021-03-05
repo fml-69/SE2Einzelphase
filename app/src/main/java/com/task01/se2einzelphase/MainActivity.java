33fd63103e34f57c02a2d609dc6e72c8078a12a9
fml-69 <gsauer22@gmail.com>
2021-03-05 14:53:49 +0100

    AndroidManifest.xml: added networking permission; MainActivity.java: added main driving code; activity_main.xml: added elements for extra task and edited existing layout for better looks; strings.xml: added values for extra task layout;

--- app/src/main/java/com/task01/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/task01/se2einzelphase/MainActivity.java
@@ -3,12 +3,35 @@ package com.task01.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button sendBtn;
+    TextView outputText;
+    EditText inputValue;
+    Networker networker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendBtn = findViewById(R.id.sendToServer);
+        sendBtn.setOnClickListener(
+                v -> {
+                    inputValue = findViewById(R.id.inputNumber);
+                    networker = new Networker(inputValue.getText().toString());
+                    networker.start();
+                    try{
+                        networker.join();
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    outputText = findViewById(R.id.outputText);
+                    outputText.setText(networker.getOutputString());
+                }
+        );
     }
 }
