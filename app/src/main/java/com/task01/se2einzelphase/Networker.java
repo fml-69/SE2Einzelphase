840886279486bbe33cfc50cbf1e1dddba5274e7b
fml-69 <gsauer22@gmail.com>
2021-03-05 14:33:31 +0100

    Create Networker.java

---
+++ app/src/main/java/com/task01/se2einzelphase/Networker.java
@@ -0,0 +1,38 @@
+package com.task01.se2einzelphase;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Networker extends Thread{
+    String inputString;
+    String outputString;
+
+    Socket clientSocket;
+    DataOutputStream outToServer;
+    BufferedReader inFromServer;
+
+    public Networker(String in){
+        this.inputString = in;
+    }
+
+    public void run(){
+        try{
+            clientSocket = new Socket("se2-isys.aau.at", 53212);
+            outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToServer.writeBytes(inputString + '\n');
+            outputString = inFromServer.readLine();
+            clientSocket.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getOutputString(){
+        return this.outputString;
+    }
+
+}
